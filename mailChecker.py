3f93a8e420054d366a1ac0de034add0dcb3b0b85
sdarends <s.d.arends@hotmail.com>
2020-11-09 18:55:29 +0100

    indicated ';' as the seperator to get the correct dataframe shape

--- mailChecker.py
+++ mailChecker.py
@@ -7,13 +7,14 @@ def load_excel(filename):
 
 
 def convert_to_pandas(csv_file):
-    df = pd.read_csv(csv_file)
+    df = pd.read_csv(csv_file, sep=';', header=0)
     return df
 
 
 def main():
     file = load_excel("Data/train_list.csv")
     table = convert_to_pandas(file)
+    print(table.shape)
     print(table)
 
 
